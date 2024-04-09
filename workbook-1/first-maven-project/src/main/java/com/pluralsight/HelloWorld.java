b37975b95bec63889ea38c56803f8258f4317bff
Natalia G <ngranado15@gmail.com>
2024-04-09 11:09:24 -0400

    completed ShoppingList app

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class HelloWorld {
+    // This is the MAIN method -entry to JVM
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
